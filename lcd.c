67b133e20ed6d68d6fd547c624e12bafd41283fd
kipperni10 <82174136+kipperni10@users.noreply.github.com>
2026-04-21 14:47:00 -0300

    Update lcd.c

--- lcd.c
+++ lcd.c
@@ -1,52 +1,69 @@
-#include "lcd.h"
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+#include "display.h"
 
-// Frequência do oscilador (Necessário para a função __delay_ms)
-// !!!!! dtem ajustar isso no main.h ou aqui se for diferente de 20MHz
-#define _XTAL_FREQ 20000000 
-
-void lcd_cmd(char cmd) {
-    LCD_DATA = cmd;
-    LCD_RS = 0; // 0 para Comando
-    LCD_EN = 1;
-    __delay_ms(2);
-    LCD_EN = 0;
+void display_pulse_enable() {
+	PORTB |= (1 << PB1);  _delay_us(1);
+	PORTB &= ~(1 << PB1); _delay_us(100);
 }
 
-void lcd_char(char data) {
-    LCD_DATA = data;
-    LCD_RS = 1; // 1 para Dados (Caracteres)
-    LCD_EN = 1;
-    __delay_ms(2);
-    LCD_EN = 0;
+void display_send_4bits(uint8_t valor, uint8_t tipo) {
+	if (tipo) PORTB |= (1 << PB0); else PORTB &= ~(1 << PB0);
+	PORTB &= ~((1 << PB2) | (1 << PB3) | (1 << PB4));
+	PORTC &= ~(1 << PC3);
+	if (valor & 0x01) PORTB |= (1 << PB2);
+	if (valor & 0x02) PORTB |= (1 << PB3);
+	if (valor & 0x04) PORTB |= (1 << PB4);
+	if (valor & 0x08) PORTC |= (1 << PC3);
+	display_pulse_enable();
 }
 
-void lcd_init(void) {
-    __delay_ms(20);
-    lcd_cmd(0x38); // Configura display para 8 bits, 2 linhas, fonte 5x7
-    lcd_cmd(0x0C); // Liga display, desliga cursor
-    lcd_cmd(0x06); // Incrementa o cursor para a direita
-    lcd_cmd(0x01); // Limpa o display
-    __delay_ms(2);
+void display_comando(uint8_t comando) {
+	display_send_4bits(comando >> 4, 0);
+	display_send_4bits(comando & 0x0F, 0);
+	_delay_ms(2);
 }
 
-void lcd_string(const char *str) {
-    while(*str != '\0') {
-        lcd_char(*str);
-        str++;
-    }
+void display_dado(uint8_t dado) {
+	display_send_4bits(dado >> 4, 1);
+	display_send_4bits(dado & 0x0F, 1);
+	_delay_us(50);
 }
 
-void lcd_clear(void) {
-    lcd_cmd(0x01);
-    __delay_ms(2);
+void display_string(const char *texto) {
+	while (*texto) display_dado(*texto++);
 }
 
-void lcd_set_cursor(char row, char col) {
-    char pos;
-    if(row == 1) {
-        pos = 0x80 + col; // Linha 1 começa no endereço 0x80
-    } else if(row == 2) {
-        pos = 0xC0 + col; // Linha 2 começa no endereço 0xC0
-    }
-    lcd_cmd(pos);
+void display_limpar() {
+	display_comando(0x01);
+	_delay_ms(2);
+}
+
+void display_posiciona(uint8_t linha, uint8_t coluna) {
+	uint8_t endereco = (linha == 0) ? (0x80 + coluna) : (0xC0 + coluna);
+	display_comando(endereco);
+}
+
+void display_on() {
+	display_comando(0x0C);
+	PORTC |= (1 << PC4);
+}
+
+void display_off() {
+	display_comando(0x08);
+	PORTC &= ~(1 << PC4);
+}
+
+void inicializa_display() {
+	DDRB |= (1 << PB0) | (1 << PB1) | (1 << PB2) | (1 << PB3) | (1 << PB4);
+	DDRC |= (1 << PC3) | (1 << PC4);
+	_delay_ms(50);
+	display_send_4bits(0x03, 0); _delay_ms(5);
+	display_send_4bits(0x03, 0); _delay_us(150);
+	display_send_4bits(0x03, 0);
+	display_send_4bits(0x02, 0);
+	display_comando(0x28);
+	display_off();
+	display_limpar();
 }
