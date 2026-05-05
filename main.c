77eb8d4ec2e6df571a2d5579f4d1be699bbecf38
kipperni10 <82174136+kipperni10@users.noreply.github.com>
2026-05-05 20:12:38 -0300

    Update main.c
    ultimo validado

--- main.c
+++ main.c
@@ -1,20 +1,19 @@
-/*
- * PROJETO 1 - MICROPROCESSADORES (MicPay)
- * Arquivo: main.c
- */
+// Projeto 1 - MicPay - Bianca Bitencourt, Henrique Bitencourt e Nicolas da Silveira Kipper //
 
-#define F_CPU 16000000UL
-#include <avr/io.h>
-#include <util/delay.h>
-#include <string.h>
+// Arquivo: main
+
+#define F_CPU 16000000UL // 16MHz, Unsigned (somente positivos) Long (32 bits)
+#include <avr/io.h> // portas
+#include <util/delay.h> // pausas e atrasos
+#include <string.h> // textos
 #include "display.h"
 #include "teclado.h"
 #include "transacao.h"
 #include "relogio.h"
 #include "admin_menu.h"
 
-typedef enum { DESLIGADO, LOGIN, MENU_PRINCIPAL, MENU_ADMIN } t_estado;
-t_estado estado_atual = DESLIGADO;
+typedef enum { DESLIGADO, LOGIN, MENU_PRINCIPAL, MENU_ADMIN } estados_sistema; // enum da nomes aos números inteiros
+estados_sistema estado_atual = DESLIGADO; // sistema inicia com o display desligado
 
 int main(void) {
     configura_pinos_teclado();
@@ -22,90 +21,87 @@ int main(void) {
     relogio_init(); 
     
     while(1) {
-        switch(estado_atual) {
+        
+        if (estado_atual == DESLIGADO) {
+            if (tecla_pressionada_bruta('#')) { // # = tecla para ligar o sistema
+                uint16_t tempo = 0; // uint16_t para inteiro, positivo e 16 bits (necessário por ser 3seg, 300)
+                while (tecla_pressionada_bruta('#') && tempo < 300) { // manter o botão de ligar pressionado por 3seg
+                    tempo++; 
+                    _delay_ms(10);
+                }
+                if (tempo >= 300) {
+                    display_on();
+                    estado_atual = LOGIN;
+                    while(tecla_pressionada_bruta('#')); 
+                }
+            }
+        }
+        
+        else if (estado_atual == LOGIN) {
+            display_limpar();
+            display_string("SENHA ACESSO:"); 
+            display_posiciona(1, 0); // 1 = linha inferior, 0 = primeira posição à esquerda
             
-            case DESLIGADO:
-                if (tecla_pressionada_bruta('#')) {
-                    uint16_t tempo = 0;
-                    while (tecla_pressionada_bruta('#') && tempo < 300) {
-                        tempo++; 
-                        _delay_ms(10);
-                    }
-                    if (tempo >= 300) {
-                        display_on();
-                        estado_atual = LOGIN;
-                        while(tecla_pressionada_bruta('#')); 
-                    }
-                }
-                break;
-
-            case LOGIN:
-                display_limpar();
-                display_string("SENHA ACESSO:"); 
-                display_posiciona(1, 0);
-                
-                char senha_op[5];
-                if (!le_dados_cliente(senha_op, 4, 1, 1)) {
-                    estado_atual = DESLIGADO;
-                    display_limpar(); display_off();
-                    break;
-                }
-
-                if (strcmp(senha_op, "0738") == 0) {
+            char senha_op[5]; // 4 dígitos + \0
+            if (!le_dados_cliente(senha_op, 4, 1, 1)) { // caso a leitura de dados falhe // 4 = n° de dígitos, 1 = ocultar com *, 1 = encerra a leitura logo que o 4° dígito for pressionado
+                estado_atual = DESLIGADO;
+                display_limpar(); 
+                display_off();
+            } else {
+                if (strcmp(senha_op, "0738") == 0) { // senha administrador
                     display_limpar(); display_string("ADMINISTRADOR"); _delay_ms(1000);
                     estado_atual = MENU_ADMIN;
                 } 
-                else if (strcmp(senha_op, "1254") == 0) {
+                else if (strcmp(senha_op, "1254") == 0) { // senha operador 0
                     if (is_operador_ativo(0)) {
-                        display_limpar(); display_string("BEM-VINDO OP0!"); _delay_ms(1000);
+                        display_limpar(); display_string("BEM-VINDO OP0!"); _delay_ms(1000); 
                         estado_atual = MENU_PRINCIPAL;
                     } else {
-                        display_limpar(); display_string("OP BLOQUEADO"); _delay_ms(2000);
+                        display_limpar(); display_string("OP BLOQUEADO"); _delay_ms(2000); // senha certa, mas acesso bloqueado pelo administrador 
                     }
                 } 
-                else if (strcmp(senha_op, "2349") == 0) {
+                else if (strcmp(senha_op, "2349") == 0) { // senha operador 1
                     if (is_operador_ativo(1)) {
                         display_limpar(); display_string("BEM-VINDO OP1!"); _delay_ms(1000);
                         estado_atual = MENU_PRINCIPAL;
                     } else {
-                        display_limpar(); display_string("OP BLOQUEADO"); _delay_ms(2000);
+                        display_limpar(); display_string("OP BLOQUEADO"); _delay_ms(2000); // senha certa, mas acesso bloqueado pelo administrador 
                     }
                 } 
                 else {
-                    display_limpar(); display_string("SENHA INVALIDA"); _delay_ms(2000);
+                    display_limpar(); display_string("SENHA INVALIDA"); _delay_ms(2000); // quando inserido qualquer outra senha de 4 dígitos não válida
                 }
-                break;
+            }
+        }
+        
+        else if (estado_atual == MENU_ADMIN) {
+            char res_admin = fluxo_menu_admin(); // admin_menu
+            if (res_admin == 'X') { // caso o administrador desligue
+                estado_atual = DESLIGADO;
+                display_limpar(); display_off();
+            } else {
+                estado_atual = LOGIN; // caso o administrador volte ou saia
+            }
+        }
+        
+        else if (estado_atual == MENU_PRINCIPAL) { // operado por operador 0 ou 1 
+            display_limpar(); 
+            display_string("1-VISTA 2-PARCEL"); 
+            display_posiciona(1, 0); 
+            display_string("3-ESTORNO *-SAIR"); 
 
-            case MENU_ADMIN: { // <-- Chaves adicionadas aqui!
-                char res_admin = fluxo_menu_admin();
-                if (res_admin == 'X') {
-                    estado_atual = DESLIGADO;
-                    display_limpar(); display_off();
-                } else {
-                    estado_atual = LOGIN; 
-                }
-                break;
-            } // <-- Fechamento das chaves aqui!
-
-            case MENU_PRINCIPAL:
-                display_limpar();
-                display_string("1-VISTA 2-PARCEL"); 
-                display_posiciona(1, 0);
-                display_string("3-ESTORNO *-SAIR"); 
-
-                char op = espera_tecla_com_shutdown();
-                
-                if (op == 'X') { 
-                    estado_atual = DESLIGADO;
-                    display_limpar(); display_off();
-                } 
-                else if (op == '*') { 
-                    estado_atual = LOGIN;
-                }
-                else if (op == '1') fluxo_venda_vista();
-                else if (op == '2') fluxo_venda_parcelada();
-                else if (op == '3') fluxo_estorno();
-                break;
+            char op = espera_tecla_com_shutdown(); // congela a tela no menu principal até ter uma ação
+            
+            if (op == 'X') { // desligar
+                estado_atual = DESLIGADO;
+                display_limpar(); display_off();
+            } 
+            else if (op == '*') { // cancelar
+                estado_atual = LOGIN;
+            }
+            else if (op == '1') fluxo_venda_vista(); // a vista
+            else if (op == '2') fluxo_venda_parcelada(); // parcelado
+            else if (op == '3') fluxo_estorno(); // estornar
         }
     }
     return 0;
